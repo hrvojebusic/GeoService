dd802eb7eddb066ed5e08a6f2e705e452220ef9c
hbusic <hrvoje.busic@infobip.com>
2016-08-23 14:23:45 +0200

    Started test.

---
+++ src/test/java/com/infobip/location/LocationAnalyzerTest.java
@@ -0,0 +1,14 @@
+package com.infobip.location;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LocationAnalyzerTest {
+
+    @Test
+    public void test() throws Exception {
+        
+    }
+}
