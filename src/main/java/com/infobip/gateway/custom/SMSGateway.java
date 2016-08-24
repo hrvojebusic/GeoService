5cd343f7caa39ec8e8f3395751ac5bb86565775b
vbilic <valter.bilic@infobip.hr>
2016-08-24 09:45:07 +0200

    before returning message response

--- src/main/java/com/infobip/gateway/custom/SMSGateway.java
+++ src/main/java/com/infobip/gateway/custom/SMSGateway.java
@@ -1,5 +1,13 @@
 package com.infobip.gateway.custom;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+
 public class SMSGateway {
-    
+
+    @Autowired
+    HttpEntity httpEntity;
+
+    private HttpHeaders headers = new HttpHeaders();
 }
