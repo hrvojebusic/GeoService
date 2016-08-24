75c443271afe821ee4bbd8cfa006eae834a17b2e
grumin <goran.rumin@infobip.com>
2016-08-24 11:41:28 +0200

    geoMessageController edit

--- src/main/java/com/infobip/controllers/GeoMessageController.java
+++ src/main/java/com/infobip/controllers/GeoMessageController.java
@@ -1,19 +1,27 @@
 package com.infobip.controllers;
 
 import com.infobip.controllers.model.MessageRequest;
+import com.infobip.location.LocationAnalyzer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@RestController
+@RequestMapping("/messages")
 public class GeoMessageController {
+
+    @Autowired
+    private LocationAnalyzer locationAnalyzer;
 
     @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity sendMessage(@Validated @RequestBody MessageRequest request) {
-            return ResponseEntity.ok().build();
+            return ResponseEntity.ok(locationAnalyzer.notifyPersonsForPolygon(request));
     }
 }
