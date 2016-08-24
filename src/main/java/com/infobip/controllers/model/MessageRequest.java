a837dfee80ebdec50b0be910162c4d9227f6822f
grumin <goran.rumin@infobip.com>
2016-08-24 09:43:57 +0200

    Refactoring, added geoMessageController, validation

--- src/main/java/com/infobip/controllers/model/MessageRequest.java
+++ src/main/java/com/infobip/controllers/model/MessageRequest.java
@@ -3,15 +3,22 @@ package com.infobip.controllers.model;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 @Setter(AccessLevel.PRIVATE)
 public class MessageRequest {
 
+    @NotEmpty
     private String sender;
 
+    @NotEmpty
     private String message;
 
+    @NotNull
     private PolygonResource polygon;
 
     protected MessageRequest(){}
