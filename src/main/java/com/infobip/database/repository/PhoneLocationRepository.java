9f109cca69d12612d970276d0a6d41352d578083
hbusic <hrvoje.busic@infobip.com>
2016-08-26 10:01:17 +0200

    Basic async features.

--- src/main/java/com/infobip/database/repository/PhoneLocationRepository.java
+++ src/main/java/com/infobip/database/repository/PhoneLocationRepository.java
@@ -2,21 +2,28 @@ package com.infobip.database.repository;
 
 import com.infobip.database.model.PhoneLocation;
 import org.springframework.data.geo.Polygon;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.Repository;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.stereotype.Repository;
 import org.springframework.util.concurrent.ListenableFuture;
 
 import java.util.List;
 
-@Repository(value = "phones")
-@EnableAsync
-public interface PhoneLocationRepository extends MongoRepository<PhoneLocation, String> {
+public interface PhoneLocationRepository extends Repository<PhoneLocation, String> {
+
+    List<PhoneLocation> findAll();
+
+    @Async
+    ListenableFuture<PhoneLocation> save(PhoneLocation phoneLocation);
 
     @Async
     ListenableFuture<PhoneLocation> findById(String id);
 
     @Async
     ListenableFuture<List<PhoneLocation>> findByLocationWithin(Polygon polygon);
+
+    void deleteAll();
+
+    void delete(String id);
+
+    PhoneLocation findOne(String id);
 }
