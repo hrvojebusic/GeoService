88700c05c9ba5fb91283545c457da3f7aa29a77b
hbusic <hrvoje.busic@infobip.com>
2016-08-26 08:51:44 +0200

    Small code refactoring.

--- src/main/java/com/infobip/config/MongoConfiguration.java
+++ src/main/java/com/infobip/config/MongoConfiguration.java
@@ -1,10 +1,7 @@
 package com.infobip.config;
 
-import com.infobip.database.model.PhoneLocation;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
-import com.mongodb.async.client.MongoCollection;
-import com.mongodb.async.client.MongoDatabase;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -20,11 +17,7 @@ public class MongoConfiguration extends AbstractMongoConfiguration {
 
     @Override
     public Mongo mongo() throws Exception {
-        MongoClient mongoClient = new MongoClient("127.0.0.1", 27017);
-
-        mongoClient.getDB("geoDB").getCollection("phones");
-
-        return mongoClient;
+        return new MongoClient("127.0.0.1", 27017);
     }
 
     @Override
